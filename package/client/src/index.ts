ccb60e52f6fa1cc209627d7664ae168e08fb0f02
Cola <bcad1591@gmail.com>
2023-01-12 06:08:21 +0900

    rename: DB -> IDB

--- package/client/src/index.ts
+++ package/client/src/index.ts
@@ -1,20 +1,24 @@
 import '@styles/index.scss';
 import Home from '@components/Home';
 import Router from '@core/Router';
+import IDB from '@IDB/index';
 
 import { initTheme } from './theme';
 
 const $App = document.querySelector('#App') as HTMLDivElement;
 
 initTheme();
+const db = new IDB('todo');
 
-new Router($App, [
-  {
-    path: '/',
-    matchHandler: () => new Home($App, {}),
-  },
-  {
-    path: '/activities',
-    matchHandler: () => new Home($App, {}),
-  },
-]);
+db.init().then(() => {
+  new Router($App, [
+    {
+      path: '/',
+      matchHandler: () => new Home($App, { db }),
+    },
+    {
+      path: '/activities',
+      matchHandler: () => new Home($App, { db }),
+    },
+  ]);
+});
