ea06ea401039e88b2117d49936c15ebbf11c0749
Cola <bcad1591@gmail.com>
2023-01-16 04:10:11 +0900

    feat: Icons 컴포넌트 추가
    - BarIcon: 메뉴
    - LightThemeIcon: 태양
    - DarkThemeIcon: 달

---
+++ package/client/src/components/Icons/index.ts
@@ -0,0 +1,11 @@
+export const BarIcon = (cn = '') => `
+  <i class="fa-solid fa-bars ${cn}"></i>
+`;
+
+export const LightThemeIcon = (cn = '') => `
+  <i class="fa-solid fa-sun ${cn}"></i>
+`;
+
+export const DarkThemeIcon = (cn = '') => `
+  <i class="fa-solid fa-moon ${cn}"></i>
+`;
