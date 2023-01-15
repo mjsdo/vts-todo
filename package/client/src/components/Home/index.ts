275779f70265d0a711ed909c796fdcf221031bbf
Cola <bcad1591@gmail.com>
2023-01-16 04:33:50 +0900

    feat: Header 컴포넌트 추가

--- package/client/src/components/Home/index.ts
+++ package/client/src/components/Home/index.ts
@@ -1,9 +1,18 @@
+import Header from '@components/Header';
 import Component from '@core/Component';
 
 export default class Home extends Component {
   render() {
     return `
-      Home
+      <section class="bg-card max-w-600 text-text trs-color">
+        <div class="header"></div>
+      </section>
     `;
+  }
+
+  appendChildComponent() {
+    const $header = this.$('.header') as HTMLDivElement;
+
+    new Header($header);
   }
 }
