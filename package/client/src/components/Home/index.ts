59d73cd7b2d3492d8e37acec8fbc2fa639ae98ef
Cola <bcad1591@gmail.com>
2023-01-16 09:35:23 +0900

    feat: PageNavigation 추가

--- package/client/src/components/Home/index.ts
+++ package/client/src/components/Home/index.ts
@@ -1,18 +1,23 @@
 import Header from '@components/Header';
+import PageNavigation from '@components/PageNavigation';
 import Component from '@core/Component';
 
 export default class Home extends Component {
   render() {
     return `
-      <section class="bg-card max-w-600 text-text trs-color">
-        <div class="header"></div>
+      <section class="w-full">
+        <header id="header" class="bg-card text-text trs-color"></header>
+        <main id="main"></main>
+        <div id="page-navigation" class="bg-card text-text max-w-600 w-600 fixed bottom-0 trs-color"></div>
       </section>
     `;
   }
 
   appendChildComponent() {
-    const $header = this.$('.header') as HTMLDivElement;
+    const $header = this.$('#header') as HTMLDivElement;
+    const $pageNav = this.$('#page-navigation') as HTMLDivElement;
 
     new Header($header);
+    new PageNavigation($pageNav);
   }
 }
---
+++ package/client/src/components/PageNavigation/index.ts
@@ -0,0 +1,48 @@
+import { HistoryIcon, TodoListIcon } from '@components/Icons';
+import Component from '@core/Component';
+import { wrap } from '@core/Component/util';
+import { useRouter } from '@core/Router';
+
+import { classnames as cn } from '../../util/dom';
+
+const pages = [
+  {
+    name: 'home',
+    href: '/',
+    component: TodoListIcon,
+  },
+  {
+    name: 'log',
+    href: '/log',
+    component: HistoryIcon,
+  },
+];
+
+export default class PageNavigation extends Component {
+  render() {
+    const { path } = useRouter().context;
+    const iconCn = '';
+
+    return `
+      <nav>
+        <ul class="flex">
+          ${wrap(pages).map(({ href, component, name }) => {
+            const liCn = cn({
+              'border-accent': href === path,
+              'border-transparent': href !== path,
+            });
+
+            return `
+                <li class="w-1-2 border-t-4 border-solid ${liCn} trs-border-color">
+                  <a route href="${href}" class="w-full h-48 flex items-center justify-center">
+                    ${component(iconCn)}
+                    <span class="sr-only">${name}</span>
+                  </a>
+                </li>
+              `;
+          })}
+        </ul>
+      </nav> 
+    `;
+  }
+}
