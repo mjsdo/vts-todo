0e1f3796266eed22c408eb19f1a60766cd07f169
Cola <bcad1591@gmail.com>
2023-01-16 12:05:06 +0900

    refactor: Icons에 aria-hidden 추가

--- package/client/src/components/Icons/index.ts
+++ package/client/src/components/Icons/index.ts
@@ -1,19 +1,19 @@
 export const BarIcon = (cn = '') => `
-  <i class="fa-solid fa-bars ${cn}"></i>
+  <i class="fa-solid fa-bars ${cn}" aria-hidden="true"></i>
 `;
 
 export const LightThemeIcon = (cn = '') => `
-  <i class="fa-solid fa-sun ${cn}"></i>
+  <i class="fa-solid fa-sun ${cn}" aria-hidden="true"></i>
 `;
 
 export const DarkThemeIcon = (cn = '') => `
-  <i class="fa-solid fa-moon ${cn}"></i>
+  <i class="fa-solid fa-moon ${cn}" aria-hidden="true"></i>
 `;
 
 export const TodoListIcon = (cn = '') => `
-  <i class="fa-solid fa-list-ol ${cn}"></i>
+  <i class="fa-solid fa-list-ol ${cn}" aria-hidden="true"></i>
 `;
 
 export const HistoryIcon = (cn = '') => `
-  <i class="fa-solid fa-clock-rotate-left ${cn}"></i>
+  <i class="fa-solid fa-clock-rotate-left ${cn}" aria-hidden="true"></i>
 `;
