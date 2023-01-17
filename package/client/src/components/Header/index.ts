759b8470dfc88d42a8646d4113dcb514b79a44a0
Cola <bcad1591@gmail.com>
2023-01-18 03:09:49 +0900

    feat: TAB_INDEX 적용

--- package/client/src/components/Header/index.ts
+++ package/client/src/components/Header/index.ts
@@ -1,4 +1,5 @@
 import { BarIcon, DarkThemeIcon, LightThemeIcon } from '@components/Icons';
+import { TAB_INDEX } from '@constants/css';
 import Component from '@core/Component';
 
 import { getBodyThemeAttr, setBodyThemeAttr } from '@/theme';
@@ -22,13 +23,15 @@ export default class Header extends Component<State> {
       <div class="px-40 bg-card text-text flex items-center justify-between" data-app-topbar>
          <h1 class="text-s24 font-sans">VTS-TODO</h1>
          <div>
-           <button type="button" class="p-4" data-theme-button>
+           <button type="button" class="p-4" tabindex="${
+             TAB_INDEX.TOP_BAR
+           }" data-theme-button>
              ${
                theme === 'dark' ? DarkThemeIcon(textCn) : LightThemeIcon(textCn)
              }
              <span class="sr-only">테마 변경</span>
            </button>
-           <button type="button" class="p-4">
+           <button type="button" class="p-4" tabindex="${TAB_INDEX.TOP_BAR}">
              ${BarIcon(textCn)}
              <span class="sr-only">애플리케이션 메뉴</span>
            </button>
