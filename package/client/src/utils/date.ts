5c30ca7c0a3806b149aabbb0bb9a3917204f5c84
Cola <bcad1591@gmail.com>
2023-01-19 02:13:34 +0900

    refactor: formatDateToKRLocale -> formatDateToKRLocaleString으로 이름 변경

--- package/client/src/utils/date.ts
+++ package/client/src/utils/date.ts
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const formatDateToKRLocale = (date: Date) =>
+export const formatDateToKRLocaleString = (date: Date) =>
   new Intl.DateTimeFormat('ko-KR').format(date).replace(/\s/g, '');
