6077f4c4071ae74ebbdb9f521cec66ddcfa913b3
Cola <bcad1591@gmail.com>
2023-01-23 22:07:37 +0900

    feat: Modal 컴포넌트 추가

--- package/client/src/components/Home/index.ts
+++ package/client/src/components/Home/index.ts
@@ -1,9 +1,11 @@
 import type TodoStorage from '@storage/TodoStorage';
 
 import Gnb from '@components/Gnb';
+import Modal from '@components/Modal';
 import TodoLayer from '@components/TodoLayer';
 import TopBar from '@components/TopBar';
 import Component from '@core/Component';
+import modalStore from '@stores/modalStore';
 
 import './styles.scss';
 
@@ -18,6 +20,7 @@ export default class Home extends Component<unknown, Props> {
         <header class="top-bar min-w-396"></header>
         <main class="main bg-background px-24 min-w-396 relative"></main>
         <nav class="gnb fixed bottom-0 shadow-xl min-w-396 max-w-600 w-screen"></nav> 
+        <section class="modal font-sans"></section>
       </section>
     `;
   }
@@ -28,9 +31,11 @@ export default class Home extends Component<unknown, Props> {
     const $topBar = this.$<HTMLElement>('.top-bar');
     const $main = this.$<HTMLElement>('.main');
     const $gnb = this.$<HTMLElement>('.gnb');
+    const $modal = this.$<HTMLElement>('.modal');
 
     new TopBar($topBar);
     new TodoLayer($main, { todoStorage });
     new Gnb($gnb);
+    modalStore.subscribe('modal', new Modal($modal));
   }
 }
