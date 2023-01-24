494181bf886c357a102cd391237d41de2d186c77
Cola <bcad1591@gmail.com>
2023-01-24 15:01:45 +0900

    refactor: Layout 컴포넌트 추가

--- package/client/src/components/Home/index.ts
+++ package/client/src/components/Home/index.ts
@@ -1,6 +1,7 @@
 import type TodoStorage from '@storage/TodoStorage';
 
 import Gnb from '@components/Gnb';
+import Layout from '@components/Layout';
 import Modal from '@components/Modal';
 import TodoLayer from '@components/TodoLayer';
 import TopBar from '@components/TopBar';
@@ -17,9 +18,7 @@ export default class Home extends Component<unknown, Props> {
   render() {
     return `
       <section class="w-full h-screen bg-background shadow-xl">
-        <header class="top-bar min-w-396"></header>
-        <main class="main bg-background px-24 min-w-396 relative"></main>
-        <nav class="gnb fixed bottom-0 shadow-xl min-w-396 max-w-600 w-screen"></nav> 
+        ${Layout()}
         <section class="modal font-sans"></section>
       </section>
     `;
---
+++ package/client/src/components/Layout/index.ts
@@ -0,0 +1,7 @@
+const Layout = () => `
+    <header class="top-bar min-w-396"></header>
+    <main class="main bg-background px-24 min-w-396 relative"></main>
+    <nav class="gnb fixed bottom-0 shadow-xl min-w-396 max-w-600 w-screen"></nav>
+  `;
+
+export default Layout;
